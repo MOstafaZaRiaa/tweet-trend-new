069b850144d1616925c9246a5d228b7917584f64
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2024-07-16 07:08:22 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if there are any running containers
+running_containers=$(docker ps -q)
+
+if [ -n "$running_containers" ]; then
+  echo "Stopping and removing running containers..."
+
+  # Stop all running containers
+  docker stop $running_containers
+
+  # Remove all containers
+  docker rm $running_containers
+else
+  echo "No running containers found."
+  docker run -dt --name tweet -p 8000:8000 zariaa.jfrog.io/namg-docker-local/tweet-trend:2.1.2
+fi
+
+# Create a new container from the specified image
+
+echo "New container created from the image 'your_image_name'."
