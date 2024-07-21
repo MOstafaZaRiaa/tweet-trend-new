6d2fee278cdc5deb35718c3c4660b74615fe4821
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2024-07-22 00:25:14 +0300

    edit k8s command

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 minikube start --force
-minikube kubectl -- apply -f namespace.yml
-minikube kubectl -- apply -f secret.yml
-minikube kubectl -- apply -f deployment.yml
-minikube kubectl -- apply -f service.yml
+kubectl apply -f namespace.yml
+kubectl apply -f secret.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
